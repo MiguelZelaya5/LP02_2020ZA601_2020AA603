ada52ff3a200484b8eaf8be18752fc678c786c06
jefferson <jefferson.avelar@catolica.edu.sv>
2023-04-22 08:58:41 -0600

    modelos

---
+++ LP02_2020ZA601_2020AA603/Models/pedidos.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace LP02_2020ZA601_2020AA603.Models
+{
+    public class pedidos
+    {
+        [Key]
+        public int pedidoId { get; set; }
+
+        public  int? motoristaId { get; set; }
+        public int? clienteId { get; set; }
+
+        public int? platoId { get; set; }
+        public int? cantidad { get; set; }
+        public double? precio { get; set; }
+
+
+    }
+}
---
+++ LP02_2020ZA601_2020AA603/Models/platos.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace LP02_2020ZA601_2020AA603.Models
+{
+    public class platos
+    {
+        [Key]
+        public int platoId { get; set; }
+        public string? nombrePlato { get; set; }
+        public double precio { get; set; }
+    }
+}
