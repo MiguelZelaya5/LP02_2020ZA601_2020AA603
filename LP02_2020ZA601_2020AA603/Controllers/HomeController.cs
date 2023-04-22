3b36fa7e4b1ab8f9bb8972760e580c9bc16f063b
MiguelZelaya5 <miguel.zelaya@catolica.edu.sv>
2023-04-22 08:38:08 -0600

    Agregar archivos de proyecto.

---
+++ LP02_2020ZA601_2020AA603/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using LP02_2020ZA601_2020AA603.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace LP02_2020ZA601_2020AA603.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ LP02_2020ZA601_2020AA603/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LP02_2020ZA601_2020AA603.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
