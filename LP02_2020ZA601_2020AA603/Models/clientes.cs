aa50fa358bff4af5a7fd79a12ba631dd23727afd
MiguelZelaya5 <miguel.zelaya@catolica.edu.sv>
2023-04-22 09:07:05 -0600

    tablas

---
+++ LP02_2020ZA601_2020AA603/Models/clientes.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LP02_2020ZA601_2020AA603.Models
+{
+    public class clientes
+    {
+        [Key]
+        public int clienteId { get; set; }
+        public string nombreCliente { get; set; }
+        public string direccion { get; set; }
+    }
+}
---
+++ LP02_2020ZA601_2020AA603/Models/motoristas.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LP02_2020ZA601_2020AA603.Models
+{
+    public class motoristas
+    {
+        [Key]
+        public int motoritaId {get; set;}
+        public string nombreMotorista { get; set;}
+    }
+}
